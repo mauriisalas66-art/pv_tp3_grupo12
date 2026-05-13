09fb39841210eb8b70837aaa489a59eb27e121e1
mauriisalas66-art <mauriisalas66@gmail.com>
2026-05-13 15:34:13 -0300

    @SalasMauri66: Cree y organice la estructura princiapl del proyecto

--- pv_tp3_grupo12/src/main.jsx
+++ pv_tp3_grupo12/src/main.jsx
@@ -1,7 +1,9 @@
+//Cuando comienza con Mayuscula es un componente, si comienza con minuscula es un elemento html
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
+import './css/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

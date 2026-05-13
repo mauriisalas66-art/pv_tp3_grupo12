2bd75a9f55c974076cd4b63d625472df90612339
Hernan Jairo Gerardo Almazan <jhgalmazan2@gmail.com>
2026-05-13 16:34:38 -0300

    HernanJairo:Agrego componente Footer estático

---
+++ pv_tp3_grupo12/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => { 
+    return ( 
+        <footer> 
+            <p>2026 Grupo 12 - Programación Visual - Facultad de Ingeniería UNJU</p> 
+        </footer> 
+        ); 
+};

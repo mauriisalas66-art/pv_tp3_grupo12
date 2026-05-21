db48139d27cd946fab8dff7bece2d7d78a2db892
mauriisalas66-art <mauriisalas66@gmail.com>
2026-05-20 22:53:00 -0300

    @SalasMauri66: Cambie la ruta de la imagen en header.jsx

--- pv_tp3_grupo12/src/components/Header.jsx
+++ pv_tp3_grupo12/src/components/Header.jsx
@@ -2,7 +2,7 @@ export const Header = () => {
     return ( 
     <header> 
         <div className="header-main-content"> 
-            <img src="../public/images/logo_unju.png" alt="Logo de la FI-UNJU" className="logo_unju" /> 
+            <img src="/images/logo_unju.png" alt="Logo de la FI-UNJU" className="logo_unju" /> 
             <div className="textos-header"> 
                 <h1>Portal de Proyectos FI-UNJU</h1> 
                 <p>Bienvenido a la Plataforma de Gestión de Proyectos Educativos</p>        

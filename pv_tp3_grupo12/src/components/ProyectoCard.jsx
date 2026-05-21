9b5cda6b9302f3e9c212131fb0936c3e0f634845
Noemansilla <Noemansilla@gmail.com>
2026-05-20 22:05:03 -0300

    Agrego ProyectoCard con estados dinamicos-MansillaCamila

--- pv_tp3_grupo12/src/components/ProyectoCard.jsx
+++ pv_tp3_grupo12/src/components/ProyectoCard.jsx
@@ -12,23 +12,19 @@ export const ProyectoCard = ({ proyecto, onEliminar, onVerDetalle, onCambiarEsta
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginTop: '15px' }}>
         <button 
           onClick={() => onCambiarEstado(id)} 
-          style={{ background: '#0284c7', color: 'white', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
-        >
+          style={{ background: '#0284c7', color: 'white', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
           Cambiar a {estado === "En Curso" ? "Finalizado" : "En Curso"}
         </button>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
           <button 
             onClick={() => onVerDetalle(proyecto)} 
-            style={{ flex: 1, background: '#f59e0b', color: 'black', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
-          >
+            style={{ flex: 1, background: '#f59e0b', color: 'black', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
             Ver Detalles
           </button>
           
           <button 
-            onClick={() => onEliminar(id)} 
-            style={{ flex: 1, background: '#e11d48', color: 'white', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
-          >
+            onClick={() => onEliminar(id)} style={{ flex: 1, background: '#e11d48', color: 'white', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
             Eliminar
           </button>
         </div>

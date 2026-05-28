3803dc0963fba767c955657116ef05d772a79237
Noemansilla <Noemansilla@gmail.com>
2026-05-28 01:19:37 -0300

    Corrijo sintaxis de desestructuracion y props- MansillaCamila

--- pv_tp3_grupo12/src/components/DetalleProyecto.jsx
+++ pv_tp3_grupo12/src/components/DetalleProyecto.jsx
@@ -1,22 +1,23 @@
 // src/components/DetalleProyecto.jsx
 export const DetalleProyecto = ({ proyecto, onVolver }) => {
-  const { titulo, descripcion, recursos, equipo } = proyecto;
+  const { titulo, categoria, estado, descripcion, enlaces, recursos, equipo } = proyecto;
 
-  return (
-    <div className="detalle-modal">
-      <button onClick={onVolver} style={{ marginBottom: '20px', padding: '10px', background: '#011931', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-        ← Cerrar Detalles
-      </button>
-      <h2>{titulo}</h2>
-      <p><strong>Descripción:</strong> {descripcion}</p>
-      <h3>Archivos y Recursos</h3>
+ return (
+  <div className="detalle-modal">
+     <button onClick={onVolver} style={{ marginBottom: '20px', padding: '10px', background: '#011931', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+      ← Cerrar Detalles
+    </button>
+    <h2>{titulo}</h2>
+    <p><strong>Descripción:</strong> 
+    {descripcion}</p>
+    <h3>Archivos y Recursos</h3>
+     <ul>
+      {recursos.length > 0 ? recursos.map((rec, i) => <li key={i}>{rec}</li>) : <li>No hay recursos subidos.</li>}
+     </ul>
+     <h3>Equipo de Trabajo</h3>
       <ul>
-        {recursos.length > 0 ? recursos.map((rec, i) => <li key={i}>{rec}</li>) : <li>No hay recursos subidos.</li>}
+         {equipo.length > 0 ? equipo.map((int, i) => <li key={i}><strong>{int.nombre}</strong> (Rol: {int.rol})</li>) : <li>Equipo sin asignar.</li>}
       </ul>
-      <h3>Equipo de Trabajo</h3>
-      <ul>
-        {equipo.length > 0 ? equipo.map((int, i) => <li key={i}><strong>{int.nombre}</strong> (Rol: {int.rol})</li>) : <li>Equipo sin asignar.</li>}
-      </ul>
-    </div>
-  );
-};
+      </div>
+ );
+}; 

356ffe695702f5cce848526b975187e86523e7e1
Hernan Jairo Gerardo Almazan <jhgalmazan2@gmail.com>
2026-06-04 13:06:41 -0300

    Hernan: Funcion tiempo real Dashboard

--- pv_tp3_grupo12/src/views/Dashboard.jsx
+++ pv_tp3_grupo12/src/views/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { Card, Button, Form, Row, Col } from 'react-bootstrap';
-
+import { ProyectoService } from '../services/proyectoService';
 export const Dashboard = () => {
+    const proyectos = ProyectoService.obtenerProyectos();
+    const totalProyectos = proyectos.length;
+    const proyectosEnCurso = proyectos.filter(p => p.estado === "En Curso").length;
     return (
         <div>
             <h1 className="mb-4">Bienvenido al Gestor de Proyectos</h1>
@@ -8,9 +11,9 @@ export const Dashboard = () => {
                 <Col md={6}>
                 <Card className="mb-4 shadow-sm">
                     <Card.Body>
-                        <Card.Title>Metricas Generales</Card.Title>
-                        <Card.Text>Ttoal de proyectos cargados: <strong>5</strong></Card.Text>
-                        <Card.Text>Proyectos en curso: <strong className="text-primary">3</strong></Card.Text>
+                        <Card.Title>Métricas Generales</Card.Title>
+                        <Card.Text>Total de proyectos cargados: <strong>{totalProyectos}</strong></Card.Text>
+                        <Card.Text>Proyectos en curso: <strong className="text-primary">{proyectosEnCurso}</strong></Card.Text>
                     </Card.Body>
                 </Card>
                 </Col>

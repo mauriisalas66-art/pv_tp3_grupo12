a9452797e8f3014c685bcdf7695af082afcca586
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2026-06-11 03:05:58 -0300

    solo pueden entrar los usuarios logiados - Ignacio Cardozo

--- pv_tp3_grupo12/src/components/RutaProtegida.jsx
+++ pv_tp3_grupo12/src/components/RutaProtegida.jsx
@@ -5,11 +5,9 @@ import { UsuarioContext } from '../context/UsuarioContext';
 export const RutaProtegida = ({ children }) => {
   const { usuario } = useContext(UsuarioContext);
 
-  // Si no está logueado, lo redireccionamos forzadamente a la raíz (Dashboard)
   if (!usuario) {
     return <Navigate to="/" replace />;
   }
 
-  // Si está validado, se le permite renderizar la vista hija (children)
   return children;
 };

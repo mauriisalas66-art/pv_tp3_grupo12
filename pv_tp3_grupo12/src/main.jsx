d4e3db8b45e86ee7fef6a378960422ccb97b428c
nievesales <nievesaless45@gmail.com>
2026-06-10 21:14:58 -0300

    configuración inicial de react-router y vista de dashboard LuisNieves

--- pv_tp3_grupo12/src/main.jsx
+++ pv_tp3_grupo12/src/main.jsx
@@ -1,5 +1,3 @@
-//Cuando comienza con Mayuscula es un componente, si comienza con minuscula es un elemento html
-// src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -8,19 +6,13 @@ import './css/index.css';
 
 import { App } from './App.jsx';
 import { Dashboard } from './views/Dashboard.jsx';
-import { ListaProyectos } from './views/ListaProyectos.jsx';
-import { DetallesProyecto } from './views/DetallesProyecto.jsx';
-import { PerfilUsuario } from './views/PerfilUsuario.jsx';
 
 const rutas = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <Dashboard /> },
-      { path: "proyectos", element: <ListaProyectos /> },
-      { path: "proyectos/:id", element: <DetallesProyecto /> },
-      { path: "perfil", element: <PerfilUsuario /> }
+      { index: true, element: <Dashboard /> } // Libre para poder loguearse
     ]
   }
 ]);

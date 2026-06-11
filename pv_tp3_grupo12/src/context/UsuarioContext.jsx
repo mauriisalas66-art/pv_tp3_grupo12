b058b07ed6ba8cb1172fa655c4818737134f27ab
mauriisalas66-art <mauriisalas66@gmail.com>
2026-06-10 23:13:18 -0300

    @salasMauri66: Añadi UsuarioContext.jsx

---
+++ pv_tp3_grupo12/src/context/UsuarioContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState,useEffect } from "react";
+export const UsuarioContext = createContext ();
+export const UsuarioProvider = ({children}) => {
+    const [usuario, setUsuario] = useState (() => {
+        const datosGuardados = localStorage.getItem ("perfil_usuario");
+        return datosGuardados ? json.parse (datosGuardados) : null;
+        //Arranca en null si no hay sesión
+    });
+
+    useEffect (() => {
+        if (usuario) {
+            localStorage.setItem("perfil_usuario", JSON.stringify(usuario));
+        }else {
+            localStorage.remnoveItem("perfil_usuario");
+        }
+    }, [usuario] );
+
+    const guardarSesion = (datos) => setUsuario(datos);
+    const cerrarSesion = () => setUsuario(null);
+    const actualizarPerfil = (nuevosDatos) => setUsuario(nuevosDatos);
+    return (
+        <UsuarioContext.Provider value={{usuario, guardarSesion, cerrarSesion, actualizarPerfil}}>
+            {children}
+        </UsuarioContext.Provider>
+    );
+};

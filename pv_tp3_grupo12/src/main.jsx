380f89a948a80472fc2084a6deef5391544aa9b9
nievesales <nievesaless45@gmail.com>
2026-06-10 21:38:54 -0300

    implemento UsuarioProvider y protección de rutas privadas LuisNieves

--- pv_tp3_grupo12/src/main.jsx
+++ pv_tp3_grupo12/src/main.jsx
@@ -10,21 +10,37 @@ import { ListaProyectos } from './views/ListaProyectos.jsx';
 import { DetallesProyecto } from './views/DetallesProyecto.jsx';
 import { PerfilUsuario } from './views/PerfilUsuario.jsx';
 
+// IMPORTACIONES DE INFRAESTRUCTURA
+import { UsuarioProvider } from './context/UsuarioContext.jsx';
+import { RutaProtegida } from './components/RutaProtegida.jsx';
+
 const rutas = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { index: true, element: <Dashboard /> },
-      { path: "proyectos", element: <ListaProyectos /> },
-      { path: "proyectos/:id", element: <DetallesProyecto /> },
-      { path: "perfil", element: <PerfilUsuario /> }
+      { index: true, element: <Dashboard /> }, // Libre para poder loguearse
+      { 
+        path: "proyectos", 
+        element: <RutaProtegida><ListaProyectos /></RutaProtegida> 
+      },
+      { 
+        path: "proyectos/:id", 
+        element: <RutaProtegida><DetallesProyecto /></RutaProtegida> 
+      },
+      { 
+        path: "perfil", 
+        element: <RutaProtegida><PerfilUsuario /></RutaProtegida> 
+      }
     ]
   }
 ]);
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={rutas} />
+    {/* Envolvemos las rutas con el proveedor global */}
+    <UsuarioProvider>
+      <RouterProvider router={rutas} />
+    </UsuarioProvider>
   </React.StrictMode>,
 );

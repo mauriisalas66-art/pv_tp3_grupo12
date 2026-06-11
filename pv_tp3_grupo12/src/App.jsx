9905fdc3d8b8b9473140128d26bfd5eb12bfdf02
Noemansilla <Noemansilla@gmail.com>
2026-06-10 23:44:19 -0300

    implemento rederizado de ruta-MansillaCamila

--- pv_tp3_grupo12/src/App.jsx
+++ pv_tp3_grupo12/src/App.jsx
@@ -1,9 +1,12 @@
-//src/App.jsx
-import { Outlet, Link} from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import {Footer} from "./components/Footer";
+import { Footer } from "./components/Footer";
+import { useContext } from 'react';
+import { UsuarioContext } from './context/UsuarioContext';
 
 export const App = () => {
+  const { usuario, cerrarSesion } = useContext(UsuarioContext);
+
   return (
     <div className="min-vh-100 d-flex flex-column">
       <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
@@ -13,15 +16,21 @@ export const App = () => {
           <Navbar.Collapse id="menu-basico">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-              <Nav.Link as={Link} to="/proyectos">Proyectos</Nav.Link>
-              <Nav.Link as={Link} to="/perfil">Perfil</Nav.Link>
+              {usuario && (
+                <>
+                  <Nav.Link as={Link} to="/proyectos">Proyectos</Nav.Link>
+                  <Nav.Link as={Link} to="/perfil">Perfil</Nav.Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
       <Container className="flex-grow-1">
         <Outlet />
       </Container>
+      
       <Footer />
     </div>
   );
